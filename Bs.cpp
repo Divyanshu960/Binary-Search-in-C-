89c46cf916d8f455b100f7ccd49c0e5242e7eaa6
Divyanshu Chaurasiya <72182690+Divyanshu960@users.noreply.github.com>
2022-01-05 02:00:34 +0530

    Update Bs.cpp

--- Bs.cpp
+++ Bs.cpp
@@ -1,19 +1,37 @@
 #include <stdio.h>
+void bs(int,int,int,int)//function prototype declared
+int main()
+{   
+    int n;
+    printf("Enter the size of the array");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i = 0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter an element to be searched");
+    int tar;
+    scanf("%d",&tar);
+    bs(arr , 0 , n-1 ,tar);
+    
+    return 0;
+}
 void bs(int arr[], int l , int h, int tar)
 {   int mid;
-    if(h==-1)
+    if(h==-1)//checks if array size is out of bound
     {
         printf("Invalid Size");
     }
     else
     {
-        if(l>h)
+        if(l>h)//compares low and high in the search
         {
             printf("Element not found");
         }
         else{
             mid = (l+h)/2;
-            if(arr[mid] == tar)
+            if(arr[mid] == tar)//checks if element found at middle
             {
                 printf("Element found at position : %d",mid);
             }
@@ -26,21 +44,5 @@ void bs(int arr[], int l , int h, int tar)
                 bs(arr, l , mid-1,tar);
             }
         }
-
     }
-}
-int main()
-{   int n;
-    printf("Enter the size of the array");
-    scanf("%d",&n);
-    int arr[n];
-    for(int i = 0; i<n; i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    printf("Enter an element to be searched");
-    int tar;
-    scanf("%d",&tar);
-    bs(arr , 0 , n-1 ,tar);
-    return 0;
 }
